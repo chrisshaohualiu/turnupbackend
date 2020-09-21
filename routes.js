ca81ec64d286c0b105cc6bf3a82ed76dbcb231c3
Chris <chrisshaohualiu@gmail.com>
2020-09-21 16:13:33 -0400

    added delete to favorite videos

--- routes.js
+++ routes.js
@@ -10,7 +10,7 @@ const pool = require("./connection");
 const { response } = require("express");
 
 routes.get("/recent", (req, res)=>{
-    pool.query(`SELECT * FROM recent`).then(response=>{
+    pool.query(`SELECT * FROM recent ORDER BY id DESC`).then(response=>{
         res.json(response.rows);
     })
 })
@@ -22,13 +22,13 @@ routes.post("/recent", (req, res)=>{
 })
 
 routes.delete("/recent/:id", (req, res)=>{
-	pool.query(`DELETE FROM favorite_artists WHERE id=${req.params.id}`).then(response=>{
+	pool.query(`DELETE FROM recent WHERE id=${req.params.id}`).then(response=>{
 		res.sendStatus(204);
 	})
 })
 
 routes.get("/favorite-artists", (req, res)=>{
-    pool.query(`SELECT * FROM favorite_artists`).then(response=>{
+    pool.query(`SELECT * FROM favorite_artists ORDER BY id DESC`).then(response=>{
         res.json(response.rows);
     })
 })
@@ -46,7 +46,7 @@ routes.delete("/favorite-artists/:id", (req, res)=>{
 })
 
 routes.get("/favorite-videos", (req, res)=>{
-    pool.query(`SELECT * FROM favorite_videos`).then(response=>{
+    pool.query(`SELECT * FROM favorite_videos ORDER BY id DESC`).then(response=>{
         res.json(response.rows);
     })
 })
@@ -55,6 +55,12 @@ routes.post("/favorite-videos", (req, res)=>{
     pool.query(`INSERT INTO favorite_videos (title, thumbnail, artist, videoId) VALUES ($1::VARCHAR, $2::VARCHAR, $3::VARCHAR, $4::VARCHAR)`, [req.body.title, req.body.thumbnail, req.body.artist, req.body.videoId]).then(response=>{
         res.json(req.body);
     })
+})
+
+routes.delete("/favorite-videos/:id", (req, res)=>{
+	pool.query(`DELETE FROM favorite_videos WHERE id=${req.params.id}`).then(response=>{
+		res.sendStatus(204);
+	})
 })
 
 module.exports = routes;
